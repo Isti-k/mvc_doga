dac1c3402814ed28afec0c52f0b580283a091910
Isti-k <kiscserkes03@gmail.com>
2024-10-01 10:19:58 +0200

    mvc

---
+++ view/Kartya.js
@@ -0,0 +1,30 @@
+
+
+
+export default class Kartya{
+    #szuloelem;
+    #index;
+    #elem;
+
+    constructor(szuloelem, index, elem){
+        this.#szuloelem=szuloelem;
+        this.#elem=elem;
+        this.#index=index;
+    }
+
+
+    megjelenit(){
+        `<div class=lap>
+            <p>${this.#elem}</p>
+        </div>`
+    }
+
+
+    kattintas(){
+        this.#kattintas.on("click" (lista) =>{
+
+        })
+    }
+
+}
+
